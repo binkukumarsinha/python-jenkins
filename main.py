3152a9d2566c8be0521a79feecf9f36b64fcb7f7
BinkuKumarSinha <202545694+binkukumarsinha@users.noreply.github.com>
2025-06-27 22:07:08 +0530

    This is v1

---
+++ main.py
@@ -0,0 +1,6 @@
+# This is for testing Jenkins integration with Python
+print("Hello, Jenkins!")
+num1 = 10
+num2 = 20
+sum_result = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum_result}.")
